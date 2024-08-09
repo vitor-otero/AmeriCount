518cbd3ee23d8ffba6c4fe6f88c8c6bab5d0cd63
Vitor Otero <vitor@v0t.org>
2024-08-09 10:38:21 +0100

    Initial Commit

---
+++ AmeriCount/AmeriCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmeriCountApp.swift
+//  AmeriCount
+//
+//  Created by Vitor Otero on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AmeriCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
